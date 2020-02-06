15e34b67af8ce7f2f44e7c6cb69dd09af1abdebf
ElyosCaesar <ElyosCaesar@DESKTOP-7AUGHBB>
2020-02-06 11:04:57 +0330

    fsrfw

---
+++ www/precache-manifest.a233298df4dcac2edce15c4c0b639be9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc338cc73f8ecf6d2db7447c51767c2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5baed92817cf78a079db",
+    "url": "/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "4e1507d7ee19a8e40cf8",
+    "url": "/static/css/main.726e81ea.chunk.css"
+  },
+  {
+    "revision": "5baed92817cf78a079db",
+    "url": "/static/js/2.d4986b35.chunk.js"
+  },
+  {
+    "revision": "4e1507d7ee19a8e40cf8",
+    "url": "/static/js/main.3a9c99c3.chunk.js"
+  },
+  {
+    "revision": "1c389d128abec7f10140",
+    "url": "/static/js/runtime-main.bb8914e0.js"
+  }
+]);
